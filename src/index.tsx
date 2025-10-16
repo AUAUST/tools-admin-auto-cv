4508b05f8eff924bb815444fa01d2323e9da5b02
apademide <contact@apademide.ch>
2025-10-16 15:19:08 +0200

    css: add fonts, autoload files

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 import App from "./App.tsx";
-import "./index.css";
+
+import.meta.glob("./styles/*.css", { eager: true });
 
 render(() => <App />, document.getElementById("root")!);
